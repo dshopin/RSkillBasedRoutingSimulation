5a156bd958a670b5fd4711f4c41171da693b00e8
dshopin <dmitry.shopin@gmail.com>
2017-09-22 15:01:16 -0700

    clean up
    keep this version, no improvement in branch

--- SBR_simulation.R
+++ SBR_simulation.R
@@ -1,9 +1,9 @@
-tic("Total")
+
 rm(list=ls())
 setwd('~\\GitHub\\RSkillBasedRoutingSimulation')
 
 
-ntask <- 5000
+ntask <- 100
 
 main.skills <- c('ZoneA','ZoneB','ZoneD','ZoneE','ZoneG','ZoneGov','ZoneI','ZoneM','ZoneQ','ZoneR','ZoneS','ZoneV')
 overflow.skills <- c('ZoneAOverflow','ZoneBOverflow','ZoneDOverflow','ZoneEOverflow','ZoneGOverflow','ZoneGovOverflow','ZoneIOverflow','ZoneMOverflow','ZoneQOverflow','ZoneROverflow','ZoneSOverflow','ZoneVOverflow','DeptSouthOverflow','DeptNorthOverflow')
@@ -57,7 +57,6 @@ servers <- read.csv(file='asm.csv', fileEncoding = 'UTF-8-BOM')
 servers <- cbind(servers,data.frame(idle.time=rep(0,nrow(servers)), release.time=rep(NA,nrow(servers)), task_id=rep(NA,nrow(servers))))
 
 queue <- data.frame(task_id=as.numeric(NULL), enqueue.time=as.numeric(NULL), service.time=as.numeric(NULL), overflow.time=as.numeric(NULL), skill=as.character(NULL), stringsAsFactors = FALSE)
-# queue <- data.frame(task_id=1, enqueue.time=1, service.time=1, overflow.time=1, skill='ZoneA', stringsAsFactors = FALSE)
 
 
 
@@ -70,28 +69,7 @@ next.event <- 0
 clock <- 0
 
 
-timer <- data.frame(task=as.numeric(NULL)
-                    ,releases=as.numeric(NULL)
-                    ,release.time=as.numeric(NULL)
-                    ,overflows=as.numeric(NULL)
-                    ,overflow.time=as.numeric(NULL)
-                    ,transfers=as.numeric(NULL)
-                    ,transfer.time=as.numeric(NULL)
-                    ,startqlen=as.numeric(NULL)
-                    ,total=as.numeric(NULL))
-
 for(row in 1:nrow(tasks)){
-  
-  releases <- 0
-  release.time <- 0
-  overflows <- 0
-  overflow.time <- 0
-  transfers <- 0
-  transfer.time <- 0
-  startqlen <- nrow(queue)
-  
-  t01 <- Sys.time()
-  
   
   while(clock < tasks[row,'arrival.time']){
     
@@ -99,9 +77,7 @@ for(row in 1:nrow(tasks)){
     #######################
     #  Releasing servers  #
     #######################
-    t1 <- Sys.time()
     for (s in servers[servers$release.time==clock & !is.na(servers$task_id),'server_id']){
-      releases <- releases + 1
       
       serverstat <- rbind(serverstat,data.frame(server_id=s, event='release', clock=clock, task_id=servers[servers$server_id==s,'task_id'], stringsAsFactors = FALSE))
       
@@ -109,15 +85,12 @@ for(row in 1:nrow(tasks)){
       servers[servers$server_id==s,'release.time'] <- NA
       servers[servers$server_id==s,'idle.time'] <- 0
     }
-    t2 <- Sys.time()
-    release.time <- release.time + (t2-t1)
+    
     
     ###################################
     #Overflow those reached thresholds#
     ###################################
-    t1 <- Sys.time()
     for (t in queue[queue$overflow.time==clock & !is.na(queue$overflow.time),'task_id']){
-      overflows <- overflows + 1
       
       overflow.skill <- ofr[ofr$skill==queue[queue$task_id==t,'skill'], 'overflow.skill']
       overflow.threshold <- ofr[ofr$skill==overflow.skill, 'threshold']
@@ -128,21 +101,17 @@ for(row in 1:nrow(tasks)){
         
       queuestat <- rbind(queuestat,data.frame(task_id=t, event='overflow', clock=clock, skill=overflow.skill, stringsAsFactors = FALSE))
     }
-    t2 <- Sys.time()
-    overflow.time <- overflow.time + (t2-t1)
     
     ######################################
     #Transfer calls from queue to servers#
     ######################################
     servers <- servers[order(-servers$idle.time),] #order by descending idle time
     
-    t1 <- Sys.time()
     for(t in queue$task_id){
       
       
       available.server <- servers[is.na(servers$task_id) & servers[,queue[queue$task_id==t,'skill']]==1, 'server_id'][1]
       if (!is.na(available.server)){
-        transfers <- transfers + 1
         
         servers[servers$server_id==available.server,'task_id'] <- queue[queue$task_id==t,'task_id']
         servers[servers$server_id==available.server,'release.time'] <- clock + queue[queue$task_id==t,'service.time']
@@ -161,10 +130,6 @@ for(row in 1:nrow(tasks)){
       
     }
     
-    t2 <- Sys.time()
-    transfer.time <- transfer.time + (t2-t1)
-    
-    
     #####################
     #Find the next event#
     #####################
@@ -175,18 +140,6 @@ for(row in 1:nrow(tasks)){
     
   }
   
-  t02 <- Sys.time()
-  total <- t02-t01
-  
-  timer <- rbind(timer, data.frame(task=row
-                                   ,releases=releases
-                                   ,release.time=release.time
-                                   ,overflows=overflows
-                                   ,overflow.time=overflow.time
-                                   ,transfers=transfers
-                                   ,transfer.time=transfer.time
-                                   ,startqlen=startqlen
-                                   ,total=total))
  
   #####################
   #  New call arrives #
@@ -199,7 +152,7 @@ for(row in 1:nrow(tasks)){
   
 }
 
-sum(timer$total)
+
 
 
 
